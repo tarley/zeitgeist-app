8eaa0479d4671b9ac0d84761d8df7a017b0b51fe
Tarley <tarley.lana@gmail.com>
2017-11-13 21:52:04 +0000

    Incluindo Loading...

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,16 +7,32 @@ angular.module('starter.controllers', [])
         }
     })
     
-    .controller('HomeController', function($scope, $http){
+    .controller('HomeController', function($scope, $http, $ionicLoading){
         
         $scope.init = function(){
-            $http.get("https://www.jornalzeitgeist.com.br/api/jornal/listToApp/", { params: {} })
+            
+            $ionicLoading.show({
+               template: 'Carregando...'
+            }).then(function() {
+                console.log("Loading exibido.");
+            });
+            
+            
+            $http.get("http://www.jornalzeitgeist.com.br/api/jornal/listToApp/", { params: {} })
             .success(function(resposta){
                 console.log(resposta);
                 $scope.jornal = resposta.data;
+                
+                $ionicLoading.hide().then(function(){
+                   console.log("Ocultando o loading");
+                });
             })
             .error(function(data){
                 console.log(data);
+                
+                $ionicLoading.hide().then(function(){
+                   console.log("Ocultando o loading");
+                });
             });
         }
     });
