cee112e6941ad5388c0e8ae7aa761af64f10197b
Carina Roriz Pimenta <carinaroriz@hotmail.com>
2017-11-20 20:39:48 +0000

    Ajustes app

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,22 @@
 angular.module('starter.controllers', [])
 
-    .controller('AppController', function($scope, $ionicHistory, $ionicSideMenuDelegate){
-        $scope.closeMenu = function(){
-            $ionicHistory.nextViewOptions({disableAnimate: true});
-            $ionicSideMenuDelegate.toggleLeft();
-        }
-    })
-    
-    .controller('HomeController', function($scope, $http){
-        
-        $scope.init = function(){
-            $http.get("https://www.jornalzeitgeist.com.br/api/jornal/listToApp/", { params: {} })
-            .success(function(resposta){
+.controller('AppController', function($scope, $ionicHistory, $ionicSideMenuDelegate) {
+    $scope.closeMenu = function() {
+        $ionicHistory.nextViewOptions({ disableAnimate: true });
+        $ionicSideMenuDelegate.toggleLeft();
+    }
+})
+
+.controller('HomeController', function($scope, $http) {
+
+    $scope.init = function() {
+        $http.get("http://www.jornalzeitgeist.com.br/api/jornal/listToApp/", { params: {} })
+            .success(function(resposta) {
                 console.log(resposta);
                 $scope.jornal = resposta.data;
             })
-            .error(function(data){
+            .error(function(data) {
                 console.log(data);
             });
-        }
-    });
+    }
+});
