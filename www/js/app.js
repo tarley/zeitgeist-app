f18b87c0de129492c38eda3d44449fa339101588
Carina Roriz Pimenta <carinaroriz@hotmail.com>
2017-12-01 20:23:26 +0000

    Alteracoes app

--- www/js/app.js
+++ www/js/app.js
@@ -6,48 +6,49 @@
 angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    $ionicPlatform.ready(function() {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 })
 
-.config(function($stateProvider, $urlRouterProvider){
-  $stateProvider
-    .state('app', {
-      url: '/app',
-      abstract: true,
-      templateUrl: 'templates/menu.html',
-      controller: 'AppController'
-    })
-    .state('app.home',{
-      url: '/home',
-      views: {
-        'menuContent':{
-          templateUrl: 'templates/home.html',
-          controller: 'HomeController'
-        }
-      }
-    })
-    .state('app.search', {
-      url: '/search',
-      views: {
-        'menuContent': {
-        templateUrl: 'templates/search.html'
-        }
-      }
-    });
-    
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('app', {
+            url: '/app',
+            abstract: true,
+            templateUrl: 'templates/menu.html',
+            controller: 'AppController'
+        })
+        .state('app.home', {
+            url: '/home',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/home.html',
+                    controller: 'HomeController'
+                }
+            }
+        })
+        .state('app.search', {
+            url: '/search',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/search.html',
+                    controller: 'EdicoesController'
+                }
+            }
+        });
+
     $urlRouterProvider.otherwise('/app/home');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,36 +1,115 @@
 angular.module('starter.controllers', [])
 
-.controller('AppController', function($scope, $ionicHistory, $ionicSideMenuDelegate) {
+.controller('AppController', function($scope, $ionicHistory, $ionicSideMenuDelegate, $window) {
     $scope.closeMenu = function() {
+        $window.localStorage.setItem('cont', 'false')
+        $ionicHistory.nextViewOptions({ disableAnimate: true });
+        $ionicSideMenuDelegate.toggleLeft();
+        $window.location.reload();
+    }
+    $scope.closeMenu1 = function() {
         $ionicHistory.nextViewOptions({ disableAnimate: true });
         $ionicSideMenuDelegate.toggleLeft();
     }
 })
 
-.controller('HomeController', function($scope, $http, $ionicLoading) {
+.controller('HomeController', function($scope, $http, $ionicLoading, $window, $state) {
+
 
     $scope.init = function() {
         $ionicLoading.show({
-          template: 'Carregando...'
-        }).then(function(){
-           console.log("Exibindo loading");
+            template: 'Carregando...'
+        }).then(function() {
+            console.log("Exibindo loading");
         });
-        
-        $http.get("http://www.jornalzeitgeist.com.br/api/jornal/listToApp/", { params: {} })
+
+        // $http.get("http://www.jornalzeitgeist.com.br/api/jornal/listToApp/", { params: {} })
+        //     .success(function(resposta) {
+        //         console.log(resposta);
+        //         $scope.jornal = resposta.data;
+        //         console.log($scope.jornal)
+        //         $ionicLoading.hide().then(function() {
+        //             console.log("Ocultando o loading");
+        //         });
+        //     })
+        //     .error(function(data) {
+        //         console.log(data);
+
+        //         $ionicLoading.hide().then(function() {
+        //             console.log("Ocultando o loading");
+        //         });
+        //     });
+        $http.get("http://www.jornalzeitgeist.com.br/api/jornal/listCompleteToApp/", { params: {} })
             .success(function(resposta) {
-                console.log(resposta);
-                $scope.jornal = resposta.data;
-                
-                $ionicLoading.hide().then(function(){
-                   console.log("Ocultando o loading");
+                var cont = $window.localStorage.getItem('cont')
+                if (cont != 'true') {
+                    console.log(resposta);
+                    var index = resposta.data.jornal.length - 1;
+                    var num_edicao = resposta.data.jornal[index].num_edicao_jornal;
+                    $window.localStorage.setItem('num_edicao', num_edicao)
+                }
+                var num_edicao2 = $window.localStorage.getItem('num_edicao')
+
+                for (i = 0; i < resposta.data.jornal.length; i++) {
+                    if (resposta.data.jornal[i].num_edicao_jornal == num_edicao2) {
+                        $scope.jornal = resposta.data.jornal[i];
+                        console.log("bla")
+                    }
+                }
+
+
+                console.log($scope.jornal)
+                $ionicLoading.hide().then(function() {
+                    console.log("Ocultando o loading");
                 });
             })
             .error(function(data) {
                 console.log(data);
-                
-                $ionicLoading.hide().then(function(){
-                   console.log("Ocultando o loading");
+
+                $ionicLoading.hide().then(function() {
+                    console.log("aqui2");
+                    console.log("Ocultando o loading");
                 });
             });
+
+    }
+})
+
+.controller('EdicoesController', function($scope, $http, $ionicLoading, $window, $state) {
+
+    $scope.butao = function(edicao) {
+        $window.localStorage.setItem('cont', 'true')
+
+        $window.localStorage.setItem('num_edicao', edicao)
+            //  $window.localstorage.jornal_id = edicao;
+        $state.go('app.home', {}, { reload: true });
+        $window.location.reload();
+    }
+
+    $scope.initEndicoes = function() {
+        $ionicLoading.show({
+            template: 'Carregando...'
+        }).then(function() {
+            console.log("Exibindo loading");
+        });
+
+        $http.get("http://www.jornalzeitgeist.com.br/api/jornal/listCompleteToApp/", { params: {} })
+            .success(function(resposta) {
+                console.log(resposta);
+                $scope.jornal = resposta.data;
+                console.log($scope.jornal)
+                $ionicLoading.hide().then(function() {
+                    console.log("Ocultando o loading");
+                });
+            })
+            .error(function(data) {
+                console.log(data);
+
+                $ionicLoading.hide().then(function() {
+                    console.log("aqui2");
+                    console.log("Ocultando o loading");
+                });
+            });
+
     }
 });
